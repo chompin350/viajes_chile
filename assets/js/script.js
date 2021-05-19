fe73650355e22cdba8549a9bcdb36777481c0feb
chompin350 <chompin@gmail.com>
2021-05-19 18:12:44 -0400

    Actualiza desafio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,30 @@
 $(document).ready(function(){
 
-    // $(".navbar a").click(function(event){
-    //     event.preventDefault();
-    //     var gato = this.hash;
-    //     $("html").animate(
-    //         {
-    //             scrollTop: $(gato).offset().top,
-    //         },
-    //         800
-    //     );
-    // });
+    // FUNCION SMOOTH SCROLL
+    $(".navbar a").click(function(event){
+        event.preventDefault();
+        var gato = this.hash;
+        $("html").animate(
+            {
+                scrollTop: $(gato).offset().top,
+            },
+            800
+        );
+    });
 
+});
+
+// FUNCION PARA CARGAR LOS TOOLTIP
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+
+
+$('.animation-toggle').hover(function() {
+    var anim = $(this).attr('data-animate');
+    $(this).addClass('animated');
+    $(this).addClass(anim);
+    setTimeout(function() {
+        $('.animation-toggle').removeClass(anim);
+    }, 3000);
 });
