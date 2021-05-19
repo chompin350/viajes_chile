921e3c74e2191fb6a8a8407500f9204f660c7a06
chompin350 <chompin@gmail.com>
2021-05-19 15:40:43 -0400

    Actualiza desafio

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    // $(".navbar a").click(function(event){
+    //     event.preventDefault();
+    //     var gato = this.hash;
+    //     $("html").animate(
+    //         {
+    //             scrollTop: $(gato).offset().top,
+    //         },
+    //         800
+    //     );
+    // });
+
+});
